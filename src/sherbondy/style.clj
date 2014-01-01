04987650ea87557c50ac0d6b9c1c06e644325575
Ethan Sherbondy <sherbondye@gmail.com>
2014-01-01 18:42:00 -0500

    initial copy in source sans.

--- src/sherbondy/style.clj
+++ src/sherbondy/style.clj
@@ -2,7 +2,7 @@
   (:refer-clojure :exclude [+ - * /])
   (:require [sherbondy.constants :as c]
             [garden.arithmetic :refer [+ - * /]]
-            [garden.color :as color :refer [as-hex hsl rgb]]
+            [garden.color :as color :refer [as-hex hsl rgb rgba]]
             [garden.core :refer [css]]
             [garden.def :refer [defrule defkeyframes]]
             [garden.stylesheet :refer [at-media]]
@@ -12,8 +12,17 @@
   (+ c/half-width
      (* 3 2 c/half-width)))
 
+(def debug-fg-color
+  (rgba (conj c/fg-color 0.9)))
+
 (defn url [link]
   (str "url('" link "')"))
+
+(def left-pad
+   (* 6 c/half-width))
+
+(def max-width
+  (* 12 2 c/half-width))
 
 (css
  {:output-to "style.css"}
@@ -22,21 +31,31 @@
    :padding 0
    :line-height 1}]
  [:body
-  {:background (str (url c/bg-img-name)
-                    " "
-                    (as-hex (rgb c/bg-color)))
+  {:background {:image (url c/bg-img-name)
+                :color (rgb c/bg-color)}
    :font-size (px 16)
-   :font-family "Futura, sans"
+   :font-family "'Source Sans Pro', sans-serif"
    :position :absolute
    :width (percent 100)
    :height (percent 100)
    }]
  [:#main
-  {:background (str (url c/fg-img-name)
-                    (as-hex (rgb c/fg-color))
-                    " repeat-y")
+  {:background {:image (url c/fg-img-name)
+                :repeat "repeat-y"
+                :color ;;debug-fg-color
+                (rgb c/fg-color)
+                }
+   :border-right "32px solid #000"
    :margin-left (px left-margin)
    :min-height (percent 100)}]
  [:#content
-  {:padding (px 32)}]
+  {:padding {:left (px left-pad)
+             :top (px (* 2 c/side-len))}
+   :max-width (px max-width)}
+  [:h1
+   {:font-size (px (* 2 c/side-len))
+    :margin-bottom (px c/side-len)}]
+  [:p
+   {:font-size (px (* 3/4 c/side-len))
+    :line-height 1.5}]]
 )
